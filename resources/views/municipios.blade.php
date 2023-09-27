a5cbf264cb302e808ca7bb79dafb46de1686a1b2
karim <karimcastillogarnica@gmail.com>
2023-09-27 14:14:45 -0500

    feat: :construction: Se arreglaron vistas
    Se hizo la vista de municipio y manzana

--- resources/views/municipios.blade.php
+++ resources/views/municipios.blade.php
@@ -17,24 +17,26 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <label for="codigo" class="col-md-4 col-form-label ">{{ __('Codigo:') }}</label>
-                <input id="number" type="email" class="form-control @error('Correo') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                <div>Nombre:</div>
-
+                <form method="POST" action="{{ route('home') }}">
+                    <label for="codigo" class="col-md-4 col-form-label ">{{ __('Codigo:') }}</label>
+                    <input id="codigo" type="number" class="form-control"  autofocus>
+                    <label for="nombre" class="col-md-4 col-form-label ">{{ __('Nombre:') }}</label>
+                    <input id="nombre" type="text" class="form-control"  autofocus>
+                </form>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="submit" class="btn btn-primary">Agregar</button>
             </div>
           </div>
         </div>
     </div>
 
-    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal1">
         Editar municipio
     </button>
 
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
@@ -42,11 +44,16 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              ...
+                <form method="POST" action="{{ route('home') }}">
+                    <label for="codigo" class="col-md-4 col-form-label ">{{ __('Codigo:') }}</label>
+                    <input id="codigo" type="number" class="form-control"  autofocus>
+                    <label for="nombre" class="col-md-4 col-form-label ">{{ __('Nombre:') }}</label>
+                    <input id="nombre" type="text" class="form-control"  autofocus>
+                </form>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="submit" class="btn btn-primary">Editado</button>
             </div>
           </div>
         </div>
