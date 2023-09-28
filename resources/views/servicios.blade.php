aa14715cce0d8a12c4055e8654948ccb99009fff
karim <karimcastillogarnica@gmail.com>
2023-09-27 19:05:09 -0500

    feat: :construction: Se hicieron mas vistas
    Se creo la vista de servicios

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -2,15 +2,73 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-2">ejercitarse</div>
-        <div class="col-2">estudiar</div>
-        <div class="col-2">emprender</div>
-        <div class="col-2">emplearse</div>
-        <div class="col-2">descansar</div>
-        <div class="col-2">ejercitarse</div>
+<div class="card text-bg-dark">
+    <img src="{{asset ('imagenes/actividades.jpg')}}" alt="...">
+    <div class="card-img-overlay">
+        <div class="container">
+            <div class="sub_title">
+                Servicios que te ofrecemos
+            </div>
+            <div class="descripcion">
+                Todo esto lo tenemos para tí, en tu tiempos libres podras usar nuestros servicios a tu manera 
+                <br>y la mejor parte es que no se te cobrara, ya que estos servicios son 100% gratis.
+                <br>Esperamos que los uses bien y sean de tu agrado y beneficio.            </div>
+            <div class="parrafo_lista">
+                <div class="lista">
+                    <ul>
+                        <li>Estudiar</li>
+                        <li>Emprender</li>
+                        <li>Emplearse</li>
+                        <li>Descansar</li>
+                        <li>Ejercitarse</li>
+                        <li>Orientación</li>
+                        <li>Asesoría Jurídica</li>
+                        <li>Asesoría Psicologíca</li>
+                        <li>Lavanderia</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="question_servicios">
+                Nuestros servicios estan a la puerta de tu casa, no esperes más para utilizarlos
+                <br>Te esperamos!
+            </div>
+        </div>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+{{-- <div class="container">
+    <div class="parrafo_servicios">
+        Servicios que te ofrecemos
+        <div class="imagen_activiades">
+            <img src="{{asset ('imagenes/actividades.jpg')}}" class="imagen_modal" alt="...">
+        </div>
+        <div class="descripcion">
+            Todo esto lo tenemos para tí, en tu tiempos libres podras usar nuestros servicios a tu manera 
+            <br>y la mejor parte es que no se te cobrara, ya que estos servicios son 100% gratis.
+            <br>Esperamos que los uses bien y sean de tu agrado y beneficio.
+        </div>
+        <div class="lista">
+            <ul>
+                <li>ejercitarse</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+</div> --}}
 
 @endsection
